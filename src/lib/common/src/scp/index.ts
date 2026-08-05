601a7636350bcc82b8bd12e817e102fe4d6d0b20
SEAFork1 <121245897+SEAFork1@users.noreply.github.com>
2026-08-05 11:40:34 +0100

    Update index.ts
    Adding the following lines so that new accounts can deploy stacksets at account creation
    
                    'arn:aws:iam::*:role/ASEA-*',
                    'arn:aws:iam::*:role/stacksets-exec-*'

--- src/lib/common/src/scp/index.ts
+++ src/lib/common/src/scp/index.ts
@@ -378,6 +378,8 @@ export class ServiceControlPolicy {
                 `arn:aws:iam::*:role/${props.organizationAdminRole || 'AWSCloudFormationStackSetExecutionRole'}`,
                 `arn:aws:iam::*:role/${props.acceleratorPrefix}*`,
                 'arn:aws:iam::*:role/aws*',
+                'arn:aws:iam::*:role/ASEA-*',
+                'arn:aws:iam::*:role/stacksets-exec-*',
               ],
             },
           },
